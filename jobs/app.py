8cd94a1206fefa0b2226f834d07d1db5c0a1d619
OmarR <OmarR@asos.com>
2020-10-23 11:35:17 +0100

    Completed module

---
+++ jobs/app.py
@@ -0,0 +1,11 @@
+import routes as routes
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/jobs')
+def jobs():
+    return render_template('index.html')
